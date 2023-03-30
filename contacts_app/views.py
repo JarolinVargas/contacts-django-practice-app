026937c4bea4eb8dc4105ffca239bf00eb459260
jarolin <jarolin.vargas@infobeans.com>
2023-03-30 14:13:11 -0400

    views, static assets, template, urls, settings confic

--- contacts_app/views.py
+++ contacts_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+
+def contacts(request):
+    context = {'my_message': 'Hello, Django!'}
+    return render(request, 'contacts.html', context)
