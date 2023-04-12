a1873acc0710e0ab28fbb1fc9be889ddd0233bd3
jarolin <jarolin.vargas@infobeans.com>
2023-04-12 10:16:50 -0400

    multiple changes

--- contacts_app/models.py
+++ contacts_app/models.py
@@ -5,3 +5,7 @@ class Contact(models.Model):
   full_name = models.CharField(max_length=255)
   email = models.EmailField(null=True)
   phone = models.CharField(max_length=15)
+
+  # Add to admin dashboard
+  def __str__(self):
+    return f"{self.full_name}"
--- contacts_app/static/contacts_app/js/main.js
+++ contacts_app/static/contacts_app/js/main.js
@@ -1,7 +1,13 @@
 const contactComponents = document.querySelectorAll(".contact");
-const addButton = document.querySelector(".btn-add");
+const addButton = document.querySelector("#add-contact");
+
+addButton.addEventListener("click", () => {
+  document.querySelector("#new-contact").classList.toggle("visible")
+});
 
 contactComponents.forEach((component) => {
+  const deleteCheckbox = component.querySelector(".delete-checkbox");
+  
   component.querySelector(".btn-action").addEventListener("click", (event) => {
     event.preventDefault();
     if (component.classList.contains("save-mode")) {
@@ -10,14 +16,9 @@ contactComponents.forEach((component) => {
     component.classList.toggle("save-mode");
   });
 
-  const deleteCheckbox = component.querySelector(".delete-checkbox");
-  if(  deleteCheckbox ) {
+  if( deleteCheckbox ) {
     deleteCheckbox.addEventListener("change", () => {
       component.submit();
     });
   }
-});
-
-addButton.addEventListener("click", () => {
-  document.querySelector("#new-contact").classList.toggle("visible")
 });
